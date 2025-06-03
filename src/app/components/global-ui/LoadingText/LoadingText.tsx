c5c4bbfeb956cab2c5317275670b829b502e480d
praful-patil <ppatil1071997@gmail.com>
2025-06-03 13:29:06 +0530

    changes in loading

--- src/app/components/global-ui/LoadingText/LoadingText.tsx
+++ src/app/components/global-ui/LoadingText/LoadingText.tsx
@@ -1,20 +1,21 @@
 import './LoadingText.scss'
 
 const LoadingText = () => {
-  return (
-    <div className="fixed inset-0 z-[9999] flex flex-col justify-center items-center bg-[#f5eee7] gap-4">
-      {/* <img src="/common/loader-img.png" alt="Loader" className="w-20 h-20 object-contain" /> */}
-      <div className="loading loading01 text-[#91765a] text-4xl tracking-widest">
-        <span>L</span>
-        <span>O</span>
-        <span>A</span>
-        <span>D</span>
-        <span>I</span>
-        <span>N</span>
-        <span>G</span>
-      </div>
-    </div>
-  )
+    return (
+        <div className="fixed inset-0 z-[9999] flex flex-col justify-center items-center bg-[#f5eee7] gap-4">
+            {/* <img src="/common/loader-img.png" alt="Loader" className="w-20 h-20 object-contain" /> */}
+            <div className="loading loading01 text-[#91765a] text-2xl sm:text-3xl md:text-4xl tracking-wider sm:tracking-widest">
+                <span>L</span>
+                <span>O</span>
+                <span>A</span>
+                <span>D</span>
+                <span>I</span>
+                <span>N</span>
+                <span>G</span>
+            </div>
+
+        </div>
+    )
 }
 
 export default LoadingText
